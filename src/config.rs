97962c24af82478b14d54662da91235102e03af5
Noel <cutie@floofy.dev>
2022-04-03 18:14:42 -0700

    chore: add in github workflows, fix description of project in files

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
-// 🥅 sentry-webhook: Webhook handler to output Sentry errors into a Discord channel.
+// 🥅 sentry-webhook: Dead simple webhook worker for Sentry to output events in a Discord channel
 // Copyright 2022 Noel <cutie@floofy.dev>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
